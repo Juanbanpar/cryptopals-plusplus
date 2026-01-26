9874cd18f5168e519eca04b9eed664a8c62437db
juanbanpar <jbanga@gradiant.org>
2026-01-26 20:35:13 +0100

    Update truncated test string

--- sets/set01/ex01/ex01_test.cpp
+++ sets/set01/ex01/ex01_test.cpp
@@ -2,8 +2,8 @@
 #include <gtest/gtest.h>
 
 TEST(HexToBase64, Example) {
-  const std::string hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e";
-  const std::string expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29u";
+  const std::string hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+  const std::string expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
   EXPECT_EQ(hex_to_base64(hex), expected);
 }
 
