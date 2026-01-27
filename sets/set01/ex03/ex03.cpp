7e47d26420bb80fd84e64d306304ea6a6558467b
juanbanpar <jbanga@gradiant.org>
2026-01-27 20:11:56 +0100

    Add ex03

---
+++ sets/set01/ex03/ex03.cpp
@@ -0,0 +1,12 @@
+#include "ex03.h"
+#include "ex03.h"
+#include "../../lib/common/common.h"
+
+namespace set01::ex03 {
+
+std::pair<unsigned char, std::string> crack_single_byte_xor(const std::string &hex) {
+  return ::crack_single_byte_xor_hex(hex);
+}
+
+} // namespace set01::ex03
+  static double table[256];
---
+++ sets/set01/ex03/ex03.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "common.h"
+
+#include <string>
+#include <utility>
+
+namespace set01::ex03 {
+
+// Crack single-byte XOR. Returns pair<key, plaintext>
+std::pair<unsigned char, std::string> crack_single_byte_xor(const std::string &hex);
+
+} // namespace set01::ex03
