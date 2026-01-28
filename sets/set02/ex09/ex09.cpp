6bce4b644486dbcdf00aa5af63a138aea500d815
juanbanpar <jbanga@gradiant.org>
2026-01-28 20:31:08 +0100

    Refactor ex09 to use new common lib

--- sets/set02/ex09/ex09.cpp
+++ sets/set02/ex09/ex09.cpp
@@ -1,21 +1,14 @@
 #include "ex09.h"
-#include <stdexcept>
+#include "pkcs7.h"
 
 namespace set02::ex09 {
 
 std::vector<unsigned char> pkcs7_pad(const std::vector<unsigned char> &data, size_t block_size) {
-  if (block_size == 0) throw std::invalid_argument("block_size must be > 0");
-  size_t pad_len = block_size - (data.size() % block_size);
-  if (pad_len == 0) pad_len = block_size;
-  std::vector<unsigned char> out = data;
-  out.insert(out.end(), pad_len, static_cast<unsigned char>(pad_len));
-  return out;
+  return pkcs7::pad(data, block_size);
 }
 
 std::string pkcs7_pad_string(const std::string &s, size_t block_size) {
-  std::vector<unsigned char> data(s.begin(), s.end());
-  auto padded = pkcs7_pad(data, block_size);
-  return std::string(padded.begin(), padded.end());
+  return pkcs7::pad_string(s, block_size);
 }
 
 } // namespace set02::ex09
