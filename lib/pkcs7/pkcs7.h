e93a5eed8d3a79770035786e768d3959d654b7d5
juanbanpar <jbanga@gradiant.org>
2026-01-28 20:30:20 +0100

    Refactor common lib

---
+++ lib/pkcs7/pkcs7.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace pkcs7 {
+std::vector<unsigned char> pad(const std::vector<unsigned char>& data, size_t block_size);
+std::string pad_string(const std::string& s, size_t block_size);
+std::vector<unsigned char> unpad(const std::vector<unsigned char>& data);
+}
