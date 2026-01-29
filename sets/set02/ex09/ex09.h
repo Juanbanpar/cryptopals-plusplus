ef0a873eefb3c37bcc9ebc1e761e7b158ca4461c
juanbanpar <jbanga@gradiant.org>
2026-01-29 17:35:46 +0100

    Format ex09

--- sets/set02/ex09/ex09.h
+++ sets/set02/ex09/ex09.h
@@ -3,7 +3,8 @@
 #include <string>
 #include <vector>
 
-namespace set02 { namespace ex09 {
-std::vector<unsigned char> pkcs7_pad(const std::vector<unsigned char> &data, size_t block_size);
-std::string pkcs7_pad_string(const std::string &s, size_t block_size);
-} }
+namespace set02::ex09
+{
+    std::vector<unsigned char> pkcs7_pad(const std::vector<unsigned char> &data, size_t block_size);
+    std::string pkcs7_pad_string(const std::string &s, size_t block_size);
+} // namespace set02::ex09
--- sets/set02/ex09/ex09_main.cpp
+++ sets/set02/ex09/ex09_main.cpp
@@ -1,15 +1,17 @@
 #include "ex09.h"
 #include <iostream>
 
-int main(int argc, char **argv) {
-  if (argc < 3) {
-    std::cerr << "usage: ex09 <block-size> <text>\n";
-    return 2;
-  }
-  size_t block = std::stoul(argv[1]);
-  std::string text = argv[2];
-  auto out = set02::ex09::pkcs7_pad_string(text, block);
-  // print raw bytes
-  std::cout << out;
-  return 0;
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        std::cerr << "usage: ex09 <block-size> <text>\n";
+        return 2;
+    }
+    size_t block = std::stoul(argv[1]);
+    std::string text = argv[2];
+    auto out = set02::ex09::pkcs7_pad_string(text, block);
+    // print raw bytes
+    std::cout << out;
+    return 0;
 }
