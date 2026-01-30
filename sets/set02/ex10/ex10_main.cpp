94fc9bf1d93b57a108d074a7bf32abd4ef9f29e7
juanbanpar <jbanga@gradiant.org>
2026-01-30 11:54:15 +0100

    Code format ex10

--- sets/set02/ex10/ex10_main.cpp
+++ sets/set02/ex10/ex10_main.cpp
@@ -3,19 +3,23 @@
 #include <fstream>
 #include "common.h"
 
-int main(int argc, char** argv) {
-  std::string filename = std::string(PROJECT_SOURCE_DIR) + "/resources/set02/ex10/10.txt";
-  if (argc > 1) filename = argv[1];  
-  std::ifstream ifs(filename);
-  if (!ifs) {
-      std::cerr << "Could not open file: " << filename << std::endl;
-      return 2;
-  }
-  std::string b64, line;
-  while (std::getline(ifs, line)) b64 += line;  
-  auto cipher = base64_to_bytes(b64);
-  std::string key = "YELLOW SUBMARINE";
-  std::vector<unsigned char> iv(16, 0);  
-  std::cout << set02::ex10::decrypt_aes_128_cbc(cipher, key, iv);
-  return 0;
+int main(int argc, char **argv)
+{
+    std::string filename = std::string(PROJECT_SOURCE_DIR) + "/resources/set02/ex10/10.txt";
+    if (argc > 1)
+        filename = argv[1];
+    std::ifstream ifs(filename);
+    if (!ifs)
+    {
+        std::cerr << "Could not open file: " << filename << std::endl;
+        return 2;
+    }
+    std::string b64, line;
+    while (std::getline(ifs, line))
+        b64 += line;
+    auto cipher = base64_to_bytes(b64);
+    std::string key = "YELLOW SUBMARINE";
+    std::vector<unsigned char> iv(16, 0);
+    std::cout << set02::ex10::decrypt_aes_128_cbc(cipher, key, iv);
+    return 0;
 }
--- sets/set02/ex10/ex10_test.cpp
+++ sets/set02/ex10/ex10_test.cpp
@@ -4,17 +4,17 @@
 #include "common.h"
 #include "ex10.h"
 
-TEST(Ex10, Solution) {
-  // The solution should contain the lyrics "Play that funky music"
-  // We check substring to verify correctness without asserting exact content length/newlines
-  std::string filename = std::string(PROJECT_SOURCE_DIR) + "/resources/set02/ex10/10.txt";
-  std::ifstream ifs(filename);
-  ASSERT_TRUE(ifs.good());
-  std::string b64, line;
-  while(std::getline(ifs, line)) b64 += line;
-  auto cipher = base64_to_bytes(b64);
-  std::string key = "YELLOW SUBMARINE";
-  std::vector<uint8_t> iv(16, 0);  
-  std::string res = set02::ex10::decrypt_aes_128_cbc(cipher, key, iv);
-  EXPECT_TRUE(res.find("Play that funky music") != std::string::npos);
+TEST(DecryptAES128CBC, Solution)
+{
+    std::string filename = std::string(PROJECT_SOURCE_DIR) + "/resources/set02/ex10/10.txt";
+    std::ifstream ifs(filename);
+    ASSERT_TRUE(ifs.good());
+    std::string b64, line;
+    while (std::getline(ifs, line))
+        b64 += line;
+    auto cipher = base64_to_bytes(b64);
+    std::string key = "YELLOW SUBMARINE";
+    std::vector<uint8_t> iv(16, 0);
+    std::string res = set02::ex10::decrypt_aes_128_cbc(cipher, key, iv);
+    ASSERT_NE(res.find("Play that funky music"), std::string::npos);
 }
