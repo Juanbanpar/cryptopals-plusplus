b286563f6529e9a700dae9de7786af335bc83f66
juanbanpar <jbanga@gradiant.org>
2026-02-07 21:53:40 +0100

    Refactor ex07

--- sets/set01/ex07/ex07_main.cpp
+++ sets/set01/ex07/ex07_main.cpp
@@ -1,12 +1,14 @@
 #include "ex07.h"
 #include <iostream>
 
-int main(int argc, char **argv) {
-  if (argc < 2) {
-    std::cerr << "usage: ex07 <base64-file>\n";
-    return 2;
-  }
-  auto plain = set01::ex07::decrypt_aes_128_ecb_base64_file(argv[1]);
-  std::cout << plain;
-  return 0;
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        std::cerr << "usage: ex07 <base64-file>\n";
+        return 2;
+    }
+    auto plain = set01::ex07::decrypt_aes_128_ecb_base64_file(argv[1]);
+    std::cout << plain;
+    return 0;
 }
