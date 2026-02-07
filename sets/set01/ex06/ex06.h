f5c06ad473f8b8a20965b899448980471badb342
juanbanpar <jbanga@gradiant.org>
2026-02-07 21:51:55 +0100

    Refactor ex06

--- sets/set01/ex06/ex06.h
+++ sets/set01/ex06/ex06.h
@@ -3,9 +3,10 @@
 #include <string>
 #include <utility>
 
-namespace set01 { namespace ex06 {
-// Return {key, plaintext}
-std::pair<std::string, std::string> break_repeating_key_xor_from_base64_file(const std::string &path);
-std::pair<std::string, std::string> break_repeating_key_xor_from_base64(const std::string &b64);
+namespace set01::ex06
+{
+    // Return {key, plaintext}
+    std::pair<std::string, std::string> break_repeating_key_xor_from_base64_file(const std::string &path);
+    std::pair<std::string, std::string> break_repeating_key_xor_from_base64(const std::string &b64);
 
-} }
+} // namespace set01::ex06
--- sets/set01/ex06/ex06_test.cpp
+++ sets/set01/ex06/ex06_test.cpp
@@ -1,10 +1,11 @@
 #include "ex06.h"
-#include "../../lib/common/common.h"
+#include "common.h"
 #include <gtest/gtest.h>
 
-TEST(BreakRepeatingKeyXOR, OfficialResource) {
-  const std::string path = std::string(PROJECT_SOURCE_DIR) + "/resources/set01/ex06/6.txt";
-  auto [key, plain] = set01::ex06::break_repeating_key_xor_from_base64_file(path);
-  EXPECT_EQ(key, "Terminator X: Bring the noise");
-  EXPECT_NE(plain.find("I'm back"), std::string::npos);
+TEST(Ex06, BreakRepeatingKeyXOR)
+{
+    const std::string path = std::string(PROJECT_SOURCE_DIR) + "/resources/set01/ex06/6.txt";
+    auto [key, plain] = set01::ex06::break_repeating_key_xor_from_base64_file(path);
+    EXPECT_EQ(key, "Terminator X: Bring the noise");
+    EXPECT_NE(plain.find("I'm back"), std::string::npos);
 }
